db21c5f8396591cb22d45c8ffe5d67cf7834150d
PepeLado <126116472+PepeLado@users.noreply.github.com>
2023-02-24 15:39:09 -0300

    Add files via upload

---
+++ cvvmandos_java.js
@@ -0,0 +1,20 @@
+function eng() {
+    alert("This page will be made available soon.We apologize for the inconvenience." )
+}
+
+function ita() {
+    alert("Questa pagina è momentaneamente incompleta. Vi preghiamo scusi per l'inconveniente.")
+}
+
+function fra() {
+    alert("Cette page est momentanément incomplète. Nous nous excusons pour ce désagrément.")
+}
+
+function cas() {
+    alert("Esta página está momentaneamente incompleta. Pedimos disculpas por la inconveniencia causada.")
+}
+
+function deu() {
+    alert("Diese Seite ist im Moment unvollständig. Wir entschuldigen uns für die Unannehmlichkeiten.")
+}
+
