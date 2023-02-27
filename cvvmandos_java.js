3050b00f3cfcd376bfe16d54510f832471575c89
PepeLado <126116472+PepeLado@users.noreply.github.com>
2023-02-26 21:46:18 -0300

    Se completó la página de idiomas y aronla de informatica y se modificó el archivo de estilos

--- cvvmandos_java.js
+++ cvvmandos_java.js
@@ -11,10 +11,20 @@ function fra() {
 }
 
 function cas() {
-    alert("Esta página está momentaneamente incompleta. Pedimos disculpas por la inconveniencia causada.")
+    alert("Esta página se irá completando a medida que avance en mi nueva carrera profesional")
 }
 
 function deu() {
     alert("Diese Seite ist im Moment unvollständig. Wir entschuldigen uns für die Unannehmlichkeiten.")
 }
+
+/**function mDown(obj) {
+    obj.style.backgroundColor = "#1ec5e5";
+    obj.innerHTML = "Release Me";
+}
+  
+function mUp(obj) {
+    obj.style.backgroundColor="#D94A38";
+    obj.innerHTML="Thank You";
+}*/
 
