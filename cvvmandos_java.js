06b6cf071af1e7974d964bb3039c0e1170e51f98
PepeLado <126116472+PepeLado@users.noreply.github.com>
2023-02-27 21:47:31 -0300

    Se agregó un evento onclick para las páginas en otros idiomas

--- cvvmandos_java.js
+++ cvvmandos_java.js
@@ -2,29 +2,10 @@ function eng() {
     alert("This page will be made available soon.We apologize for the inconvenience." )
 }
 
-function ita() {
-    alert("Questa pagina è momentaneamente incompleta. Vi preghiamo scusi per l'inconveniente.")
-}
-
-function fra() {
-    alert("Cette page est momentanément incomplète. Nous nous excusons pour ce désagrément.")
-}
-
 function cas() {
-    alert("Esta página se irá completando a medida que avance en mi nueva carrera profesional")
+    alert("Esta página no existe todavía; se irá completando a medida que avance en mi nueva carrera profesional")
 }
 
-function deu() {
-    alert("Diese Seite ist im Moment unvollständig. Wir entschuldigen uns für die Unannehmlichkeiten.")
+function changeText(id) {
+    id.innerHTML = "N/A. Please go to page in Spanish";
 }
-
-/**function mDown(obj) {
-    obj.style.backgroundColor = "#1ec5e5";
-    obj.innerHTML = "Release Me";
-}
-  
-function mUp(obj) {
-    obj.style.backgroundColor="#D94A38";
-    obj.innerHTML="Thank You";
-}*/
-
